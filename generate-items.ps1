4f00bac319cc161e7246b57cb1e5757e5ba2d61e
Michael T Lombardi <michael.t.lombardi@gmail.com>
2021-08-29 12:46:23 -0500

    (MAINT) Standardize item ordering
    This commit ensures that the generation script always writes items
    ordered by category, subcategory, and then name.

--- generate-items.ps1
+++ generate-items.ps1
@@ -9,10 +9,12 @@ Begin {
   $BonusItems = Get-Content "$PSScriptRoot/data/bonus-items.yaml" -Raw
   | ConvertFrom-Yaml -Ordered
   $BonusItems = ForEach ($Item in $BonusItems) { [PSCustomObject]$Item }
+  $BonusItems = $BonusItems | Sort-Object -Property categories, name
   # Catalogue Item Retrieval
   $CatalogueItems = Get-Content "$PSScriptRoot/data/catalogue-items.yaml" -Raw
   | ConvertFrom-Yaml -Ordered
   $CatalogueItems = ForEach ($Item in $CatalogueItems) { [PSCustomObject]$Item }
+  $CatalogueItems = $CatalogueItems | Sort-Object -Property categories, name
 }
 
 Process {
